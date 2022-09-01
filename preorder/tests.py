adc0154208d9bc6fee4eaf1d66072200588724e2
welly091 <whtp1490@gmail.com>
2022-09-01 10:56:32 -0700

    preorder test added

--- preorder/tests.py
+++ preorder/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Preorder
 
-# Create your tests here.
+class MenuTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = get_user_model().objects.create_user(username="testuser1", password="uncommon")
+        testuser1.save()
+
+        test_thing = Preorder.objects.create(client=testuser1, item="MacBook Pro", prime=True,
+                                         price=1200)
+        test_thing.save()
+
+
+    def test_things_model(self):
+        preorder_1= Preorder.objects.get(id=1)
+        actual_client = str(preorder_1.client)
+        actual_item= str(preorder_1.item)
+        actual_price = str(preorder_1.price)
+        actual_prime = str(preorder_1.prime)
+        self.assertEqual(actual_client, "testuser1")
+        self.assertEqual(actual_item, "MacBook Pro")
+        self.assertEqual(actual_price, '1200')
+        self.assertEqual(actual_prime, 'True')
